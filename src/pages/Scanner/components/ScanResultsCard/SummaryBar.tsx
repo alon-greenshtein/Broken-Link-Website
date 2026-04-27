feb0ee352ab5552407fa9794ec3f6e240c24d69e
Alon Greenstein <alon996@gmail.com>
2026-04-27 14:03:43 +0300

    feat: refactor SummaryBar component to use div elements for consistency in layout

--- src/pages/Scanner/components/ScanResultsCard/SummaryBar.tsx
+++ src/pages/Scanner/components/ScanResultsCard/SummaryBar.tsx
@@ -12,12 +12,12 @@ export const SummaryBar = ({ total, timeInSeconds, isDark }: SummaryBarProps) =>
   const { t } = useTranslation();
   return (
     <div style={scanPageStyle.summaryBar(isDark)}>
-      <span>{t('scanner_page.scan_results_card.scanned_label')}</span>
-      <strong style={scanPageStyle.summaryBold(isDark)}>
+      <div>{t('scanner_page.scan_results_card.scanned_label')}</div>
+      <div style={scanPageStyle.summaryBold(isDark)}>
         {t('scanner_page.scan_results_card.link', { count: total })}
-      </strong>
-      <span>{DOT}</span>
-      <span style={scanPageStyle.summaryTime(isDark)}>{timeInSeconds}s</span>
+      </div>
+      <div>{DOT}</div>
+      <div style={scanPageStyle.summaryTime(isDark)}>{timeInSeconds}s</div>
     </div>
   );
 };
